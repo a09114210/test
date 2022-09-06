c63d06f490fdc13a6fcfcf85629283722551b542
a09114210 <a09114210@aliyun.com>
2022-09-06 18:19:42 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gittest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @authour jimmy
+ * @date 2022/9/6 0006-18:15
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
